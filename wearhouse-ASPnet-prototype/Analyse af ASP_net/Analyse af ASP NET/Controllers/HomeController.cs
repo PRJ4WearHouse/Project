1271178c477a76d9477d55792cb2b8064e09e3ca
Sigurd Skov Jensen <Sigurd.skov@hotmail.com>
2021-10-01 15:39:14 +0200

    ASP-net prototype
    Basal MVC er anvendt i prototypen

---
+++ wearhouse-ASPnet-prototype/Analyse af ASP_net/Analyse af ASP NET/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Analyse_af_ASP_NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analyse_af_ASP_NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Overvejelser()
+        {
+            return View();
+        }
+
+        public IActionResult Forsiden()
+        {
+            return View();
+        }
+
+        public IActionResult Profil()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
