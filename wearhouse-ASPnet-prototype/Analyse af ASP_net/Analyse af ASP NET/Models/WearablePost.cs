339eb3ed3cf04665b862c5b66de9ef5735d03945
skragh <83223908+skragh@users.noreply.github.com>
2021-10-14 13:18:56 +0200

    WeararblePost 0.1 uden test done.

--- wearhouse-ASPnet-prototype/Analyse af ASP_net/Analyse af ASP NET/Models/WearablePost.cs
+++ wearhouse-ASPnet-prototype/Analyse af ASP_net/Analyse af ASP NET/Models/WearablePost.cs
@@ -7,13 +7,16 @@ namespace Analyse_af_ASP_NET.Models
 {
     public class WearablePost
     {
-        //Wearable
+        public Wearable wearable { get; set; }
         public DateTime date { get; set; }
         public WearablePostState state { get; set; }
         public double? price { get; set; }
-        WearablePost()
+        WearablePost(Wearable wearable, WearablePostState state = WearablePostState.Unavailable, double? price = null)
         {
-
+            this.wearable = wearable;
+            this.date = new DateTime();
+            this.state = state;
+            this.price = price;
         }
         public void ChangeState(WearablePostState newState, double? newPrice=null)
         {
