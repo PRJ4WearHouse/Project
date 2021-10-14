a51bc6cbc6deb33fe03d7635922bef58684f3c23
skragh <83223908+skragh@users.noreply.github.com>
2021-10-14 14:26:00 +0200

    Added TShirt as implementation of abstract Class wearable

--- Prototype02/Prototype02/Prototype02/Models/Wearable.cs
+++ Prototype02/Prototype02/Prototype02/Models/Wearable.cs
@@ -9,16 +9,15 @@ namespace Prototype02.Models
         public string wearableImagePath { get; private set; }
         public string brand { get; private set; }
         public string description { get; private set; }
-        public string type { get; private set; }
+        public string type { get; set; } = default;
         public Gender gender { get; private set; }
 
-        public Wearable(uint size, string wearableImagePath, string brand, string description, string type, Gender gender)
+        public Wearable(uint size, string wearableImagePath, string brand, string description, Gender gender)
         {
             this.size = size;
             this.wearableImagePath = wearableImagePath;
             this.brand = brand;
             this.description = description;
-            this.type = type;
             this.gender = gender;
         }
     }
---
+++ Prototype02/Prototype02/Prototype02/Models/WearableClasses/TShirt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prototype02.Models.WearableClasses
+{
+    public class TShirt : Wearable
+    {
+        public TShirt(uint size, string wearableImagePath, string brand, string description, Gender gender) : base(size, wearableImagePath, brand, description, gender)
+        {
+            this.type = "T-Shirt";
+        }
+    }
+}
