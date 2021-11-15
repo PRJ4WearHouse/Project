900784e4f117557c1b748f5591204b37267fc072
au619496 <201804204@post.au.dk>
2021-11-15 03:18:33 +0100

    Removed usermanager from WearableController

--- WearHouse/WearHouse_WebApp/Controllers/WearablesController.cs
+++ WearHouse/WearHouse_WebApp/Controllers/WearablesController.cs
@@ -13,11 +13,9 @@ namespace WearHouse_WebApp.Controllers
     public class WearablesController : Controller
     {
         private readonly UnitOfWorkCreatingWearable _unitOfWork;
-        private readonly UserManager<ApplicationUser> _userManager;
 
         public WearablesController(UserManager<ApplicationUser> userManager, ApplicationDbContext context,IWebHostEnvironment hostEnvironment)
         {
-            this._userManager = userManager;
             _unitOfWork = new UnitOfWorkCreatingWearable(context, userManager, "DefaultEndpointsProtocol=https;AccountName=wearhouseimages;AccountKey=XsPSwlsWqpM67glYBUVc/d5Tm5XBKx3KTgZg3dCo6Hz2rHnz9+mQH3cmgnSLJsRK6gmDtOPEj0y0860AhGgWBw==;EndpointSuffix=core.windows.net");
         }
 
@@ -39,12 +37,10 @@ namespace WearHouse_WebApp.Controllers
             if (ModelState.IsValid)
             {
                 //Get user
-                wearable.Owner = await _userManager.GetUserAsync(HttpContext.User);
+                wearable.Owner = await _unitOfWork.GetCurrentUserWithoutWearables(HttpContext);
 
                 if(await _unitOfWork.SaveWearableWithImages(wearable))
-
-                //Redirect
-                return RedirectToAction("Profile", "Home", new {wearable.Owner.Id });
+                    return RedirectToAction("Profile", "Home", new {wearable.Owner.Id });
             }
             return View(wearable);
         }
