7e74d2d50eebddd79f06da4c0c61508871f3f540
HackTheOxidation <tomas.hagenau@protonmail.ch>
2021-11-25 14:03:31 +0100

    Added tests for Entity: dbComments.

--- WearHouse/WearHouse_WebApp.Test.Unit/Models/Entity/dbCommentsTest.cs
+++ WearHouse/WearHouse_WebApp.Test.Unit/Models/Entity/dbCommentsTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using WearHouse_WebApp.Models.Domain;
 using WearHouse_WebApp.Models.Entities;
@@ -15,5 +17,80 @@ namespace WearHouse_WebApp.Test.Unit.Models
             uut = new dbComments();
         }
 
+        [Test]
+        public void DefaultConstructor_ConvertToDomainCommentModel_AllNull_Succeeds()
+        {
+            // Arrange takes place in SetUp()
+
+            // Act
+            var actual = uut.ConvertToDomainCommentModel();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(new DateTime(), actual.Moment);
+                Assert.IsNull(actual.Comment);
+                Assert.IsNull(actual.Author);
+                Assert.AreEqual(0, actual.WearableId);
+            });
+        }
+
+        [Test]
+        public void AmbiguousConstructor_AuthorIsNull_ConvertToDomainCommentModel_Succeeds()
+        {
+            // Arrange
+            uut = new dbComments()
+            {
+                Comments = "Test Comment",
+                Moment = DateTime.Today,
+            };
+
+            // Act
+            var actual = uut.ConvertToDomainCommentModel();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("Test Comment", actual.Comment);
+                Assert.AreEqual(DateTime.Today, actual.Moment);
+                Assert.IsNull(actual.Author);
+            });
+        }
+
+        [Test]
+        public void AmbiguousConstructor_AuthorIsInitialized_ConvertToDomainCommentModel_Succeeds()
+        {
+            // Arrange
+            uut = new dbComments()
+            {
+                Comments = "Test Comment",
+                Moment = DateTime.Today,
+                Author = new ApplicationUser()
+                {
+                    Location = "San Francisco",
+                    FirstName = "Clever",
+                    LastName = "Tester",
+                    ProfileImageUrl = "profile.png",
+                    Wearables = new List<dbWearable>(),
+                    UserName = "Tester",
+                    Id = "1",
+                },
+            };
+
+            // Act
+            var actual = uut.ConvertToDomainCommentModel();
+
+            // Assert
+            Assert.Multiple(() =>
+                {
+                    Assert.AreEqual("Test Comment", actual.Comment);
+                    Assert.AreEqual(DateTime.Today, actual.Moment);
+                    Assert.AreEqual("profile.png", actual.Author.ProfileImageUrl);
+                    Assert.AreEqual("1", actual.Author.UserId);
+                    Assert.AreEqual("Clever", actual.Author.FirstName);
+                    Assert.AreEqual("Tester", actual.Author.LastName);
+                    Assert.AreEqual("Tester", actual.Author.Username);
+                });
+        }
     }
 }
