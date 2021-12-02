2550333ad4a9e3f0339ae6298ba19b7a3c9f0640
au619496 <201804204@post.au.dk>
2021-12-02 13:00:12 +0100

    Òrganiseret lidt

---
+++ WearHouse/WearHouse_WebApp.Test.Unit/HelperFunctions/UsermanagerMock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace WearHouse_WebApp.Test.Unit.HelperFunctions
+{
+    // https://stackoverflow.com/questions/49165810/how-to-mock-usermanager-in-net-core-testing
+
+    public static Mock<UserManager<TUser>> MockUserManager<TUser>(List<TUser> ls) where TUser : class
+    {
+    var store = new Mock<IUserStore<TUser>>();
+    var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+    mgr.Object.UserValidators.Add(new UserValidator<TUser>());
+    mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+
+    mgr.Setup(x => x.DeleteAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+    mgr.Setup(x => x.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<TUser, string>((x, y) => ls.Add(x));
+    mgr.Setup(x => x.UpdateAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+
+        return mgr;
+    }
+
+    private List<ApplicationUser> _users = new List<ApplicationUser>
+    {
+    new ApplicationUser(){Id = "1" },
+    new ApplicationUser(){Id = "2" },
+    new ApplicationUser(){Id = "3" }
+    };
+}
